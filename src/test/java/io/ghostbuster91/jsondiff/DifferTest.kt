f396cec0576c42754cfcb3235d2c2e1099687146
ghostbuster91 <kghost0@gmail.com>
2018-08-05 15:33:53 +0200

    shouldReturnTrueForTwoIdenticalJsons

--- src/test/java/io/ghostbuster91/jsondiff/DifferTest.kt
+++ src/test/java/io/ghostbuster91/jsondiff/DifferTest.kt
@@ -1,5 +1,23 @@
 package io.ghostbuster91.jsondiff
 
+import org.junit.Assert
+import org.junit.Test
+
 class DifferTest {
 
+    @Test
+    fun shouldReturnTrueForTwoIdenticalJsons() {
+        val first = """{
+         "id": 1
+        }""".trimIndent()
+
+        val second = """{
+         "id": 1
+        }""".trimIndent()
+        Assert.assertTrue(compare(first,second))
+    }
+
+    fun compare(first: String, second: String): Boolean {
+        return true
+    }
 }
