01853aaf4f2b66bea854dd6fc5ca32ef16b896b1
ghostbuster91 <kghost0@gmail.com>
2018-08-13 09:48:12 +0200

    [Refactor] Rename printer to formatter

--- src/main/java/io/ghostbuster91/jsondiff/ResultsPrinter.kt
+++ src/main/java/io/ghostbuster91/jsondiff/ResultsPrinter.kt
@@ -1,63 +1,62 @@
 package io.ghostbuster91.jsondiff
 
 import com.google.gson.GsonBuilder
-import com.google.gson.LongSerializationPolicy
 
 fun printResults(diffResults: List<DiffResult>, displayOptions: DisplayOptions) {
     println("Found ${diffResults.size} differences\n")
-    val printer = PrettyPrinter(ValuePrinter(displayOptions))
+    val formatter = PrettyJsonFormatter(ValueFormatter(displayOptions))
     diffResults.forEachIndexed { id, item ->
         println("Diff number ${id + 1}:")
-        printSingleResult(item, printer)
+        printSingleResult(item, formatter)
     }
 }
 
-private fun printSingleResult(diffResult: DiffResult, printer: PrettyPrinter) {
+private fun printSingleResult(diffResult: DiffResult, formatter: Formatter) {
     diffResult.let {
         println("Affected key: ${it.key}")
-        println("First value: ${printer.apply(it.firstValue)}")
-        println("Second value: ${printer.apply(it.secondValue)}")
-        println("Where first belongs to \n${printer.apply(it.firstObject)}")
-        println("And second belongs to \n${printer.apply(it.secondObject)}")
+        println("First value: ${formatter.format(it.firstValue)}")
+        println("Second value: ${formatter.format(it.secondValue)}")
+        println("Where first belongs to \n${formatter.format(it.firstObject)}")
+        println("And second belongs to \n${formatter.format(it.secondObject)}")
         println("====================================================================================")
         print("\n\n")
     }
 }
 
-private interface Printer {
-    fun apply(any: Any?): Any?
+private interface Formatter {
+    fun format(any: Any?): Any?
 }
 
-private class ValuePrinter(private val displayOptions: DisplayOptions) : Printer {
-    override fun apply(any: Any?): Any? {
-        return applyRec(any)
+private class ValueFormatter(private val displayOptions: DisplayOptions) : Formatter {
+    override fun format(any: Any?): Any? {
+        return dispatchByType(any)
     }
 
-    private fun applyRec(any: Any?, depth: Int = 0): Any? {
+    private fun dispatchByType(any: Any?, depth: Int = 0): Any? {
         return when (any) {
-            is Map<*, *> -> applyToMap(any, depth)
-            is List<*> -> printList(any)
+            is Map<*, *> -> formatObject(any, depth)
+            is List<*> -> formatList(any)
             else -> any
         }
     }
 
-    private fun printList(list: List<*>): Any {
+    private fun formatList(list: List<*>): Any {
         return list.take(displayOptions.maxItems)
     }
 
-    private fun applyToMap(map: Map<*, *>, depth: Int): Any {
-        return map.mapValues { (_, value) -> if (depth < displayOptions.maxDepth) applyRec(value, depth) else null }
+    private fun formatObject(map: Map<*, *>, depth: Int): Any {
+        return map.mapValues { (_, value) -> if (depth < displayOptions.maxDepth) dispatchByType(value, depth) else null }
     }
 }
 
-private class PrettyPrinter(private val printer: Printer) : Printer {
+private class PrettyJsonFormatter(private val formatter: Formatter) : Formatter {
     private val gson = GsonBuilder()
             .setPrettyPrinting()
             .create()
 
-    override fun apply(any: Any?): Any? {
+    override fun format(any: Any?): Any? {
         return if (any is Map<*, *>) {
-            gson.toJson(printer.apply(any))
+            gson.toJson(formatter.format(any))
         } else {
             any
         }
